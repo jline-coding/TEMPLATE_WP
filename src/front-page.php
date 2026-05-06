748b2bc898c612188cba047e7fec14c991c49cdb
hiep011094 <hiep011094@gmail.com>
2026-05-06 13:23:45 +0700

    test

--- src/front-page.php
+++ src/front-page.php
@@ -3,7 +3,7 @@ get_header();
 ?>
 <main class="p-top">
     <div class="p-top__inner">
-        test 123
+        test
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/common/img_dummy.webp" alt="">
     </div>
 </main>
